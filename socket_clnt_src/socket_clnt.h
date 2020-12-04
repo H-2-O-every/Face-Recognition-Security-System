68d3ed5751dba043ba63df6e2483a85cf583c850
petersmith1996 <74898298+petersmith1996@users.noreply.github.com>
2020-12-04 11:21:12 +0900

    Update socket_clnt.h

--- socket_clnt_src/socket_clnt.h
+++ socket_clnt_src/socket_clnt.h
@@ -13,3 +13,4 @@ void sockInit(int* sock, struct sockaddr_in* serv_addr, int port);
 void sockConnect(int sock, struct sockaddr_in serv_addr);
 void fileAndSockProc(int sock, char image_name[100]);
 void error_handling( char* message);
+void rm_img_dir(char path[100]);//you must be careful because this func_ use [rm] with option [-rf]
