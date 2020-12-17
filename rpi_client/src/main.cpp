564777645eab535f09b79d8b477349780b645e30
petersmith1996 <74898298+petersmith1996@users.noreply.github.com>
2020-12-17 18:35:59 +0900

    Update main.cpp

--- rpi_client/src/main.cpp
+++ rpi_client/src/main.cpp
@@ -1,13 +1,20 @@
 #include "socket_clnt.h"
 
-#define GPIO_LED 24
+#define BSIZ 64
+#define IMGLEN 100
 
 int main( int argc, char* argv[] ) {
 	int sock;
 	struct sockaddr_in serv_addr;
 	char message[30];
 	int str_len;
-	
+	int fd = -1;
+//	FILE* IR;
+//	IR = fopen("IRflag", "w");
+//	fprintf(IR, "0");
+//	fclose(IR);
+
+//	fd = open("/dev/LED_IRQ", O_RDWR);	
 	in_notice(argc, argv[0]);
 	
 	printf("main : init start\n");
@@ -17,17 +24,33 @@ int main( int argc, char* argv[] ) {
 	printf("main : connect start\n");
 	sockConnect(sock, serv_addr);
 	printf("main : connect end\n");
-//while(1){	
-	char image_name[100] = {"\0"};
 
-	capt_img(image_name);
-	
-//	sockInit(&sock, &serv_addr, argv);
-	
-//	sockConnect(sock, serv_addr);
-	
-	fileAndSockProc(sock, image_name);
-//}
+	char image_name[IMGLEN] = {"\0"};
+	int intcount;
+	char buf[BSIZ];
+	int retVal=0;
+	while(1){
+		strcpy(image_name, "\0");
+
+//		intcount = read(fd, buf, BSIZ, NULL);
+		getchar();
+		retVal = -1;
+
+//		IR = fopen("IRflag", "r+");
+//		fscanf(IR, "%d", &intcount);
+//		printf("intcount : %d\n", intcount);
+//		if(intcount==1){
+			printf("capture\n");
+			capt_img(image_name);
+			fileAndSockProc(sock, image_name);
+			retVal=get_retVal(sock);
+			printf("return : %d\n", retVal);
+//			intcount = 0;
+//			fseek(IR, 0, SEEK_SET);
+//			fprintf(IR, "0");
+//		}
+//		fclose(IR);
+	}
 	close(sock);
 	return 0;
 }
